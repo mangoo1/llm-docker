911aafbe1f3f77ed5d2ddf5c1c3d29d81cd28c77
Deven Chen <devenchan@gmail.com>
2024-02-24 11:58:52 +1100

    Dockerized Gemma 7b

---
+++ gemma/gemma-7b/app.py
@@ -0,0 +1,11 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b-it")
+model = AutoModelForCausalLM.from_pretrained("google/gemma-7b-it", device_map="auto", torch_dtype=torch.bfloat16)
+
+input_text = "Write me a poem about Machine Learning."
+input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
+
+outputs = model.generate(**input_ids, max_new_tokens=1000)
+print(tokenizer.decode(outputs[0]))
